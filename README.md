77c96ef88531d25d3af0a554790cfa0870534d76
Nathan Stepper <nstepper@outlook.com>
2023-05-12 20:06:59 -0700

    allow for user input

--- README.md
+++ README.md
@@ -1,34 +1,39 @@
+
 # 09-grundy-sunday
 
-  # undefined
+A readme generator application that can quickly create a professional readme for a new project
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+clone the repo thru github and run the application with node v16 and the command line of an integrated terminal. The command to start the application is  node.js 
+
+## Usage
+
+to create a professional readme for your code repository
+
+## License
+
+[![License](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
   
-  undefined
-  
-  ## Table of Contents
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
-  - [Questions](#questions)
-  
-  ## Installation
-  
-  undefined
-  
-  ## Usage
-  
-  undefined
-  
-  ## Credits
-  
-  undefined
-  
-  ## License
-  
-  This project is licensed under the undefined license.
-  
-  ## Questions
-  
-  If you have any questions or comments, please contact undefined at undefined.
-  
+This project is licensed under the MIT license.
+
+## Contributing
+
+do a pull request and wait for approval
+
+## Tests
+
+use a variety of alphanumeric inputs to ensure it generates a readme properly
+
+## Questions
+
+If you have any questions or comments, please contact nstepper at nstepper@uw.edu.
--- index.js
+++ index.js
@@ -1,56 +1,118 @@
-// TODO: Include packages needed for this application
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-      err ? console.error(err) : console.log('README file generated successfully!')
-    );
-  }
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a brief description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions for your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How should your project be used?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license will you be using for your project?',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are your contribution guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions for your project?',
+  },
+];
+
+function generateReadme(answers) {
+  return `
+# ${answers.title}
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## License
+
+[![License](https://img.shields.io/badge/License-${
+    answers.license
+  }-brightgreen.svg)](https://opensource.org/licenses/${answers.license})
   
-// TODO: Create a function to initialize app
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+If you have any questions or comments, please contact ${
+    answers.github
+  } at ${answers.email}.
+`;
+}
+
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-      const readmeContent = `
-  # ${answers.title}
-  
-  ${answers.description}
-  
-  ## Table of Contents
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
-  - [Questions](#questions)
-  
-  ## Installation
-  
-  ${answers.installation}
-  
-  ## Usage
-  
-  ${answers.usage}
-  
-  ## Credits
-  
-  ${answers.credits}
-  
-  ## License
-  
-  This project is licensed under the ${answers.license} license.
-  
-  ## Questions
-  
-  If you have any questions or comments, please contact ${answers.github} at ${answers.email}.
-  `;
-  
-      writeToFile('README.md', readmeContent);
-    });
-  }
-// Function call to initialize app
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateReadme(answers);
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err
+        ? console.error(err)
+        : console.log('README file generated successfully!')
+    );
+  });
+}
+
 init();
